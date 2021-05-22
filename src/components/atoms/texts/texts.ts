88e75c1cc0113c7c43e129f9cbb9fd76a99cff28
Curo-Dev <admin@curo.xyz>
2021-05-22 21:03:24 +0900

    feat: ChatDateTitle 추가

--- src/components/atoms/texts/texts.ts
+++ src/components/atoms/texts/texts.ts
@@ -2,6 +2,18 @@ import styled from "styled-components";
 
 export const ChatItemTitle = styled.span`
   display: block;
-  font-size: 14px;
+  font-size: 12px;
   font-weight: 500;
+`;
+
+export const ChatDateTitle = styled.span`
+  display: block;
+  background-color: #b2b2b2;
+  color: white;
+  font-size: 12px;
+  font-weight: 500;
+  
+  border-radius: 15px;
+  padding: 2px 15px;
+  margin: 20px 0;
 `;
