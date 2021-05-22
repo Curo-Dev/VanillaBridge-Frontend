9133f8055c56a6b04394d1c2dc8945b89cc0ba50
Curo-Dev <admin@curo.xyz>
2021-05-22 21:03:08 +0900

    feat: ContextImage 적용

--- src/components/atoms/images/images.ts
+++ src/components/atoms/images/images.ts
@@ -6,3 +6,10 @@ export const ChatProfileImage = styled.img`
   display: block;
   border-radius: 10px;
 `;
+
+export const ChatContextImage = styled.img`
+  width: 100%;
+  height: auto;
+  display: block;
+  border-radius: 10px;
+`;
