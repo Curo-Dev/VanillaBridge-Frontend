f98b3f64015d339c99a362fe705b358ab361fbf2
Curo-Dev <admin@curo.xyz>
2021-05-22 19:53:02 +0900

    feat: atoms 에 images, text 추가

---
+++ src/components/atoms/images/images.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ChatProfileImage = styled.img`
+  width: 30px;
+  height: 30px;
+  display: block;
+  border-radius: 10px;
+`;
---
+++ src/components/atoms/texts/texts.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ChatItemTitle = styled.span`
+  display: block;
+  font-size: 14px;
+  font-weight: 500;
+`;
