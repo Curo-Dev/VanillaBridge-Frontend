4a02fa4c48c58f68cbfacc992c7b1b24d8eaac67
Curo-Dev <admin@curo.xyz>
2021-05-23 03:52:33 +0900

    feat: dayjs 적용

--- src/components/molecules/chat-item/chat-item.tsx
+++ src/components/molecules/chat-item/chat-item.tsx
@@ -1,14 +1,18 @@
 import React, {
   FC,
+  useRef,
 } from "react";
-import styled, {
-  css
-} from "styled-components";
+import styled from "styled-components";
 import {
   ChatProfileImage,
   ChatContextImage,
   ChatItemTitle,
+  ChatTimeTitle,
 } from "src/components/atoms";
+import dayjs from "dayjs";
+import 'dayjs/locale/ko' 
+
+dayjs.locale('ko');
 
 export type ChatItemProps = {
   id: number;
@@ -22,39 +26,17 @@ export type ChatItemProps = {
   }
 };
 
-
-type ChatItemBlockProps = {
-  isMe: boolean;
-};
-
-const ChatItemBlock = styled.div<ChatItemBlockProps>`
+const ChatItemBlock = styled.div`
   display: flex;
-  
-  ${props => props.isMe && css`
+  margin-top: 10px;
+
+  &[data-sent="true"] {
+    margin-top: 0;
     flex-direction: row-reverse;
-    img {
-      display: none;
-    }
-    div {
-      margin: 0;
-
-      span {
-        visibility: hidden;
-      }
-
-      div {
-        background-color: #8A74FF;
-        color: white;
-
-        img {
-          display: block;
-        }
-      }
-    }
-  `}
+  }
 `;
 
-const ChatItemContentBox = styled.div`
+const ChatItemBox = styled.div`
   width: auto;
   height: auto;
   display: flex;
@@ -63,9 +45,18 @@ const ChatItemContentBox = styled.div`
 `;
 
 const ChatItemChatBox = styled.div`
+  display: flex;
+  align-items: flex-end;
+  
+  div[data-sent="true"] > div > & {
+    flex-direction: row-reverse;
+  }
+`;
+
+const ChatItemContext = styled.div`
   width: auto;
   height: auto;
-  max-width: 200px;
+  max-width: 190px;
   display: flex;
   background-color: white;
   box-shadow: 1px 1px 1px #d2d2d2;
@@ -73,7 +64,19 @@ const ChatItemChatBox = styled.div`
   font-weight: 500;
   border-radius: 12px;
   padding: 5px 15px;
-  margin: 5px 0;
+  margin: 5px 5px;
+
+  
+  div[data-sent="true"] > div > div > & {
+    background-color: #8A74FF;
+    color: white;
+  }
+`
+
+const ChatProfileModal = styled.img`
+  width: 100%;
+  height: auto;
+  display: none;
 `;
 
 export const ChatItem: FC<ChatItemProps> = ({
@@ -83,20 +86,38 @@ export const ChatItem: FC<ChatItemProps> = ({
   msg,
   created_at,
 }) => {
-  
+  const imgRef = useRef<HTMLImageElement>(null);
+  const date = dayjs(created_at);
+
+  const onProfileClick = () => {
+    if(imgRef.current != null) {
+      imgRef.current.appendChild(new HTMLImageElement())
+    }
+  }
+
   return (
-    <ChatItemBlock isMe={user_id == 2} >
-      <ChatProfileImage src={photo_url} alt="" />
-      <ChatItemContentBox>
+    <ChatItemBlock data-sent={user_id === 2} >
+      <ChatProfileModal 
+        src={photo_url} />
+      <ChatProfileImage
+        ref={imgRef}
+        onClick={onProfileClick}
+        src={photo_url}
+        alt="" />
+      <ChatItemBox>
         <ChatItemTitle>{user_name}</ChatItemTitle>
         <ChatItemChatBox>
-          {msg.content && msg.content.split("\\n").map(x => {
-            return <>{x} <br /></>;
-          })}
-          {msg.mtype === "photo" && <ChatContextImage src={photo_url} alt="" />}
+          <ChatItemContext>
+            {msg.content && msg.content.split("\\n").map(x => {
+              return <>{x} <br /></>;
+            })}
+            {msg.mtype === "photo" && <ChatContextImage src={photo_url} alt="" />}
+          </ChatItemContext>
+          <ChatTimeTitle>
+            {date.format("A h:mm")}
+          </ChatTimeTitle>
         </ChatItemChatBox>
-      </ChatItemContentBox>
-
+      </ChatItemBox>
     </ChatItemBlock>
   );
 };
