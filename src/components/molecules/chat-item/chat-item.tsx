4b088f2ab3102a31950ea37759ccb87c6e41397f
Curo-Dev <admin@curo.xyz>
2021-05-22 19:53:26 +0900

    feat: type 변경 및 스타일 적용

--- src/components/molecules/chat-item/chat-item.tsx
+++ src/components/molecules/chat-item/chat-item.tsx
@@ -2,30 +2,67 @@ import React, {
   FC,
 } from "react";
 import styled from "styled-components";
+import {
+  ChatProfileImage,
+  ChatItemTitle,
+} from "src/components/atoms";
 
 export type ChatItemProps = {
+  id: number;
   user_id: number;
-  user_name: number;
+  user_name: string;
   photo_url: string;
-  created_at: Date;
+  created_at: string;
   msg: {
-    mtype: "text" | "photo";
-    content: string
+    mtype: string;
+    content?: string
   }
-}
+};
 
 const ChatItemBlock = styled.div`
+  display: flex;
+`;
 
+const ChatItemContentBox = styled.div`
+  width: auto;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
+`;
+
+const ChatItemChatBox = styled.div`
+  width: auto;
+  height: auto;
+  display: flex;
+  background-color: white;
+  font-size: 14px;
+  font-weight: 500;
+  border-radius: 12px;
+  padding: 5px 15px;
+  margin: 5px 0;
 `;
 
 export const ChatItem: FC<ChatItemProps> = ({
   user_id,
   user_name,
+  photo_url,
   msg,
+  created_at,
 }) => {
+  
   return (
-    <ChatItemBlock>
-      {msg.content}
+    <ChatItemBlock >
+      <ChatProfileImage src={photo_url} alt="" />
+      <ChatItemContentBox>
+        <ChatItemTitle>{user_name}</ChatItemTitle>
+        <ChatItemChatBox>
+          {msg.content && msg.content.split("\\n").map(x => {
+            return <>{x} <br /></>;
+          })}
+        </ChatItemChatBox>
+      </ChatItemContentBox>
+
     </ChatItemBlock>
   );
 };
