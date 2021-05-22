e58b0e0be193d56d7ed552efd5a2839edd7fab09
Curo-Dev <admin@curo.xyz>
2021-05-22 21:04:10 +0900

    feat: DateBox 적용

--- src/components/organisms/chat-list/chat-list.tsx
+++ src/components/organisms/chat-list/chat-list.tsx
@@ -6,14 +6,30 @@ import {
   ChatItem,
   ChatItemProps,
 } from "src/components/molecules";
+import {
+  ChatDateTitle,
+} from "src/components/atoms"
 
 type ChatListProps = {
   chats: ChatItemProps[]
 }
 
 const ChatListBlock = styled.div`
+  width: 100%;
+  height: auto;
   margin: 20px 10px;
   overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const ChatDateBox = styled.div`
+  width: auto;
+  height: auto;
+  display: flex;
+  justify-content: center;
 `;
 
 export const ChatList: FC<ChatListProps> = ({
@@ -21,17 +37,38 @@ export const ChatList: FC<ChatListProps> = ({
 }) => {
   return (
     <ChatListBlock>
-      {chats.map((chat, index) => (
-        <ChatItem
-          id={chat.id}
-          user_id={chat.user_id}
-          user_name={chat.user_name}
-          created_at={chat.created_at}
-          photo_url={chat.photo_url}
-          msg={chat.msg}
-          key={index}
-          />
-      ))}
+      {chats.map((chat, index) => { // 개인적으로 fromEntities 이용해서 Object 안에 넣고 Key 값 돌리고 싶은데 PASS!
+        let [prevDate] = index == 0 ? [""] : chats[index - 1].created_at.split(" "); 
+        let [date] = chat.created_at.split(" ");
+
+        const dateD = new Date(date);
+        
+        const dateStr = `${dateD.getFullYear()}년 ${dateD.getMonth()}월 ${dateD.getDate()}일`;
+
+        return (
+          <>
+            
+                {prevDate == date || 
+                <ChatDateBox>
+                  <ChatDateTitle>
+                    {dateStr}
+                  </ChatDateTitle>
+                </ChatDateBox>
+                }
+            
+            <ChatItem
+              id={chat.id}
+              user_id={chat.user_id}
+              user_name={chat.user_name}
+              created_at={chat.created_at}
+              photo_url={chat.photo_url}
+              msg={chat.msg}
+              key={index}
+              />
+          </>
+        )
+      })}
+      
     </ChatListBlock>
   );
 };
