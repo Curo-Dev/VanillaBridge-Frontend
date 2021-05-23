840c0c17e5d8e89b4fd9adfc27eab4abfac98ba3
Curo-Dev <admin@curo.xyz>
2021-05-24 05:32:44 +0900

    fix: vercel 에서 HTTP 오류 HTTPS 로 수정

--- src/modules/chat/chat.ts
+++ src/modules/chat/chat.ts
@@ -4,7 +4,7 @@ export const chatList = async () => {
   try {
     const {
       data,
-    } = await Axios.get("http://test.vanillabridge.com/test_data");
+    } = await Axios.get("https://test.vanillabridge.com/test_data");
     return data;
   } catch (e) {
     throw new Error("서버에서 데이터를 가져오는데 실패했습니다.");
